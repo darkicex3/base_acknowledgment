b1ad3082d8a22748f906c2505edc72b7674596ad
darkicex3 <maxime.junique@gmail.com>
2016-04-12 15:01:58 +0800

    init

--- apps/core/static/core/js/main.js
+++ apps/core/static/core/js/main.js
@@ -1,3 +1,26 @@
 jQuery(document).ready(function ($) {
-    $('.dropdown-toggle').dropdown();
+
+    $('#search_categories').webuiPopover({
+                        type:'async',
+                        url: GET_CATEGORIES,
+                        content:function(data){
+                            var html = '<ul>';
+                            for(var key in data){html+='<li>'+data[key]+'</li>';}
+                            html+='</ul>';
+                            return html;
+                        }, placement:'bottom', animation: 'pop', trigger: 'hover'});
+
+    $('#search_sorting').webuiPopover({
+                        type:'async',
+                        url: GET_SORTING_METHODS,
+                        content:function(data){
+                            var html = '<ul>';
+                            for(var key in data){html+='<li>'+data[key]+'</li>';}
+                            html+='</ul>';
+                            return html;
+                        }, placement:'bottom', animation: 'pop', trigger: 'hover'});
+    
+    $('#search_field').onclick(function () {
+        
+    })
 });
