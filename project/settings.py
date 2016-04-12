3a659416602eaf18381ac421b1006231bbeb7687
darkicex3 <maxime.junique@gmail.com>
2016-04-12 18:15:35 +0800

    init

--- project/settings.py
+++ project/settings.py
@@ -52,6 +52,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'django_markup',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.syndication',
     'django_mptt_admin',
 ]
 
