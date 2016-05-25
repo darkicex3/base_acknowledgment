d8401119559c0f697fe2c62489b9841cc68031a3
darkicex3 <maxime.junique@gmail.com>
2016-05-26 02:38:23 +0800

    staticfiles

--- project/wsgi.py
+++ project/wsgi.py
@@ -7,7 +7,7 @@ sys.path.append('/var/www/base_acknowledgment')
 # add the virtualenv site-packages path to the sys.path
 sys.path.append('/var/www/base_acknowledgment/env/lib/python3.4/site-packages')
 
-# poiting to the project settings
+# poting to the project settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 from django.core.wsgi import get_wsgi_application
