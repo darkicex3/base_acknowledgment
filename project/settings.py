a9f84f23840b4b662fcdb26f3ce071e3f58ed587
darkicex3 <maxime.junique@gmail.com>
2016-05-31 16:22:08 +0800

    attachments article

--- project/settings.py
+++ project/settings.py
@@ -319,3 +319,7 @@ SUMMERNOTE_CONFIG = {
     # You can disable file upload feature.
     'disable_upload': False,
 }
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
